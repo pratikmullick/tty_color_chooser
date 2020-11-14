83b8fbe3442eb82591960637c897a866ca6966b4
Pratik <pratik@alphalaptoplinux>
2020-11-14 18:33:47 +0530

    Removed TODO and moved to v0.6

--- ttycolor.py
+++ ttycolor.py
@@ -3,6 +3,7 @@
 import random
 import os
 import sys
+import subprocess
 import argparse
 import tempfile
 
@@ -14,18 +15,19 @@ class Program:
 
     def __init__(self):
         self.charset = digits + ascii_lowercase + ascii_uppercase
-
+        self.tempdir = tempfile.gettempdir()
+        
         self.name = "ttycolor"
         self.description = "A program to generate and change tty colors in Linux"
-        self.tempdir = tempfile.gettempdir()
 
         self.parser = argparse.ArgumentParser(prog=self.name, description=self.description, allow_abbrev=False)
         self.parser.add_argument("-o", "--output", type=str, metavar="OUTFILE", help="Output file name")
         self.parser.add_argument("-v", "--verbose", action="store_true", help="Prints details of output file")
-        # self.parser.add_argument("-s", "--setup", action="store_true", help="Sets up console colors")
-        self.parser.add_argument("--version", action="version", version="%(prog)s 0.5 beta")
+        self.parser.add_argument("-s", "--setup", action="store_true", help="Sets up console colors")
+        self.parser.add_argument("--version", action="version", version="%(prog)s 0.6 beta")
 
         self.parser.add_argument("filename", metavar="FILE", type=str, help="Input file in YAML format")
+        
         self.args = self.parser.parse_args()
 
         self.filename = self.args.filename
@@ -46,12 +48,18 @@ class Program:
 
     def main(self):
 
-        if not os.path.isfile(self.args.filename):
-            print("File Not Found.")
+        if not os.path.isfile(self.filename):
+            print("File not found.")
+            self.parser.print_usage()
             sys.exit()
 
         fileops = converter.FileOps(self.filename, self.outfile)
         fileops.write_svr(self.args.verbose)
+
+        if self.args.setup:
+            print("Setting up terminal colors")
+            subprocess.run(["setvtrgb", self.outfile])
+            subprocess.run(["clear"])
 
         if not self.args.output:
             print("Writing to {}".format(self.outfile))
